4761eb6de30258e431fe4cd90abe6b8c1b7ac3ed
shinobi9 <shinobi9c@outlook.com>
2025-04-07 20:24:34 +0800

    解决nightly 的挂载问题

---
+++ entrypoint-nightly.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+working_dir=/opt/server
+
+working_dir_bak=/opt/server_backup
+ 
+mkdir -p $working_dir
+
+if [ -z "$(ls -A $working_dir)" ]; then
+    echo "first run container , cp files from backup."
+    cp -r $working_dir_bak/* $working_dir
+fi
+
+rm -rf $working_dir_bak
+
+cd $working_dir && java -jar ./BeiDou.jar --spring.config.location=./application.yml $@
